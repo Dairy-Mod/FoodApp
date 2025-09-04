9536f8e57c0d11bedeab8b7be53be0eccf0d5e74
Dairy-Mod <albert-0605@outlook.com>
2025-09-03 18:35:08 -0600

    Se añadió el Header

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -5,18 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foodapp.ui.theme.FoodAppTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +55,48 @@ fun FoodApp(innerPaddingValues: PaddingValues) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(10.dp)
+            .padding(top = 10.dp)
+            .padding(start = 10.dp)
+            .padding(end = 10.dp)
     ) {
+        //Header
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(shape = CircleShape)
+                    .background(Color.White),
+                Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Icono de perfil",
+                    tint = Color.Red,
+                    modifier = Modifier.size(38.dp)
+                )
+            }
 
+            Text(
+                text = "Hola, Alberto",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = Color.DarkGray,
+                modifier = Modifier.padding(start = 5.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                imageVector = Icons.Default.ExitToApp,
+                contentDescription = "Icono de salida",
+                tint = Color.Red,
+                modifier = Modifier.size(38.dp)
+            )
+        }
     }
 }
 
